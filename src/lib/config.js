8d48c5f4bb52399df270f8c3ca6149bb5971b35a
Damien  Wade <ddw75216@gmail.com>
2026-03-11 05:53:55 -0500

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,1 +1,7 @@
-
+export const APP_CONFIG = {
+  name: "HereJustNow",
+  version: "1.0.0-mvp",
+  anchorLimitMinutes: 60, // The hard 1-hour limit
+  discoveryRadiusMiles: 10, // How far a buyer can "see"
+  isStateless: true, // Reminder: NO DATABASES ALLOWED
+};
